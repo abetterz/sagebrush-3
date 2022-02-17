bafe910bbd9b63b69350ef24d491f5a74f9af4ed
ompatel-champion <77649066+ompatel-champion@users.noreply.github.com>
2022-02-18 00:58:34 +0800

    American Industry Photo Update

--- src/components/sections/hometwo/Community.js
+++ src/components/sections/hometwo/Community.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const communityposts = [
     { colclass: "col-lg-4", link: "/agriculture-and-food-security", img: "assets/img/story-big/about-1.jpg", badge: "Nutrition", title: "Agriculture and Food Security" },
     { colclass: "col-lg-4", link: "/chinese-aggression", img: "assets/img/story-big/about-2.jpg", badge: "China", title: "Chinese Aggression" },
-    { colclass: "col-lg-4", link: "/american-industry", img: "assets/img/story-big/about-3.jpg", badge: "Economy", title: "American Industry" },
+    { colclass: "col-lg-4", link: "/american-industry", img: "assets/img/story-big/home-community-american-history.jpg", badge: "Economy", title: "American Industry" },
     { colclass: "col-lg-6", link: "/crypto-currencies", img: "assets/img/story-big/about-6.jpg", badge: "Finance", title: "Crypto Currencies" },
     { colclass: "col-lg-6", link: "/local-land-management", img: "assets/img/story-big/about-7.jpg", badge: "Land", title: "Local Land Management" }
 ];
