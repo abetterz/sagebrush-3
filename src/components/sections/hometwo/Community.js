611e9e6439e7116c1f79538d8331aa541ea30e0f
ompatel-champion <77649066+ompatel-champion@users.noreply.github.com>
2022-02-18 01:15:00 +0800

    SAGERBRUSH QA - Land photo

--- src/components/sections/hometwo/Community.js
+++ src/components/sections/hometwo/Community.js
@@ -6,7 +6,7 @@ const communityposts = [
     { colclass: "col-lg-4", link: "/chinese-aggression", img: "assets/img/story-big/about-2.jpg", badge: "China", title: "Chinese Aggression" },
     { colclass: "col-lg-4", link: "/american-industry", img: "assets/img/story-big/home-community-american-history.jpg", badge: "Economy", title: "American Industry" },
     { colclass: "col-lg-6", link: "/crypto-currencies", img: "assets/img/story-big/about-6.jpg", badge: "Finance", title: "Crypto Currencies" },
-    { colclass: "col-lg-6", link: "/local-land-management", img: "assets/img/story-big/about-7.jpg", badge: "Land", title: "Local Land Management" }
+    { colclass: "col-lg-6", link: "/local-land-management", img: "assets/img/story-big/home-community-local-land-management.jpg", badge: "Land", title: "Local Land Management" }
 ];
 
 class Community extends Component {
