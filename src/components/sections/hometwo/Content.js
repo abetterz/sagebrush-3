fbca154ba956cb6bcc99dcfc56262378b0430c42
ompatel-champion <ompatelupwork@gmail.com>
2022-02-19 07:47:42 +0800

    Eliminate a section

--- src/components/sections/hometwo/Content.js
+++ src/components/sections/hometwo/Content.js
@@ -23,7 +23,7 @@ class Content extends Component {
                 {/* <section className="section-padding our-product">
                     <Latestproduct />
                 </section> */}
-                <Advertise />
+                {/* <Advertise /> */}
                 <Donation />
                 <Campaign />
                 <Testimonials />
