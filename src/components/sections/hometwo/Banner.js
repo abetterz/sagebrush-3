4a29fca97e7c0f8c9d254412bd471350b1f496fc
ompatel-champion <ompatelupwork@gmail.com>
2022-02-26 23:05:43 +0800

    update font style and add border

--- src/components/sections/hometwo/Banner.js
+++ src/components/sections/hometwo/Banner.js
@@ -32,20 +32,22 @@ class Banner extends Component {
                     <div className="col-lg-8 align-self-center">
                       <div className="right-side-content text-center">
                         <img src={bannerLogo} style={bannerLogoStyle} className="image-fit mb-5" alt="square-logo" />
-                        <p className="text-white fw-400">
-                          Challenging modern day politics to restore our great nation's values and pioneering spirit.
-                        </p>
-                        <p className="text-white fw-400">
-                          Join us in making a difference.
-                        </p>
-                        <Link
-                          to="/shop-left"
-                          className="btn-solid with-line btn-big mt-20" style={{ display: "none" }}
-                        >
-                          <span>
-                            Shop Now <i className="fas fa-caret-right" />
-                          </span>
-                        </Link>
+                        <div className="border">
+                          <p className="text-white fw-400 font-georgia-regular">
+                            Challenging modern day politics to restore our great nation's values and pioneering spirit.
+                          </p>
+                          <p className="text-white fw-400 font-georgia-regular">
+                            Join us in making a difference.
+                          </p>
+                          <Link
+                            to="/shop-left"
+                            className="btn-solid with-line btn-big mt-20" style={{ display: "none" }}
+                          >
+                            <span>
+                              Shop Now <i className="fas fa-caret-right" />
+                            </span>
+                          </Link>
+                        </div>
                       </div>
                     </div>
                   </div>
