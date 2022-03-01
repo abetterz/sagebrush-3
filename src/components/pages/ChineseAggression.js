1c0b42fd9fa4aa39b1f3dd43ee27427f2a0e7aa3
ompatel-champion <ompatelupwork@gmail.com>
2022-03-02 07:41:38 +0800

    update chinese aggression

--- src/components/pages/ChineseAggression.js
+++ src/components/pages/ChineseAggression.js
@@ -22,17 +22,17 @@ class ChineseAggression extends Component {
                             Chinese Aggression
                         </h3>
                         <p>
-                            The Chinese Communist Party (CCP) is the greatest foreign threat to our country, and is quickly moving to become the world's most influential economic and militaristic superpower. The CCP wants to exclude the United States from the Western Pacific and Asia. It has infiltrated key American industries and institutions, especially academia and technology. Our organization seeks to eliminate China's influence on American domestic industry, innovation, research, and reverse the expansion of its power around the globe
+                            The Chinese Communist Party (CCP) is the greatest foreign threat to our country and is quickly moving to become the world's most influential economic and militaristic superpower. The CCP wants to exclude the United States from the Western Pacific and Asia. It has infiltrated key American industries and institutions, especially academia and technology. Our organization seeks to eliminate China's influence on American domestic industry, innovation, research, and reverse the expansion of its power around the globe.
                         </p>
                         <h5 className="w-100 mt-3">
-                            Policy Proposals
+                            Policy Recommendations
                         </h5>
                         <br />
                         <ul className="sagebrush-list">
-                            <li>- Penalize U.S. companies from off-shoring their manufacturing to China</li>
+                            <li>- Penalize U.S. companies from off-shoring their manufacturing to China.</li>
                             <li>- Increase tariffs on all Chinese imports that compete with American domestic industries.</li>
-                            <li>- Ban Chinese purchases of U.S. land, including by multinational corporations with ties to the CCP.</li>
-                            <li>- Place stricter parameters and penalties on publicly funded research institutions, and individuals within those institutions, pertaining to correspondence and cooperation with individuals and institutions tied to the CCP. *</li>
+                            <li>- Ban Chinese purchases of U.S. land, including by multinational corporations with ties to the CCP. </li>
+                            <li>- Place stricter parameters and penalties on publicly funded research institutions, and individuals within those institutions, pertaining to correspondence and cooperation with individuals and institutions tied to the CCP.</li>
                         </ul>
                     </div>
                 </div>
