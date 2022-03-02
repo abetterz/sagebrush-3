048f492952e06e5ebe53bfa45a64572ac19332f3
ompatel-champion <ompatelupwork@gmail.com>
2022-03-02 10:23:33 +0800

    add newsletter

--- src/components/layouts/Newslettertwo.js
+++ src/components/layouts/Newslettertwo.js
@@ -1,6 +1,38 @@
 import React, { Component } from 'react';
 
 class Newslettertwo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            res: ''
+        }
+        this.updateEmail = this.updateEmail.bind(this);
+    }
+    updateEmail(event) {
+        this.setState({ email: event.target.value });
+    }
+
+    handleClick = () => {
+        console.log(this.state.email);
+        fetch('https://public-health-backend.herokuapp.com/api/members/subscribe_sagebrush/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                fullname: '',
+                email: this.state.email,
+            })
+        })
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    res: "Success!",
+                });
+            })
+    }
     render() {
         return (
             <section className="section-padding block_newsletter bg-theme-primary">
@@ -16,12 +48,13 @@ class Newslettertwo extends Component {
                     <div className="row">
                         <div className="col-12">
                             <div className="subscribe-wrapper">
-                                <form id="subscribe-form" method="post" action="#">
+                                <div id="subscribe-form" >
                                     <div className="subscribe-content">
-                                        <input type="text" name="subscribe-input" id="subscribe-input" placeholder="Enter Your Email Address" className="form-control input-text required-entry validate-email" />
-                                        <button className="button" type="submit"><span>Subscribe</span></button>
+                                        <input type="text" name="subscribe-input" id="subscribe-input" value={this.state.email} onChange={this.updateEmail} placeholder="Enter Your Email Address" className="form-control input-text required-entry validate-email" />
+                                        <button className="button" onClick={this.handleClick}><span>Subscribe</span></button>
+                                        <p className='result text-center'>{this.state.res}</p>
                                     </div>
-                                </form>
+                                </div>
                             </div>
                         </div>
                     </div>
