382d60fdcecb13196663284bf25aa9baae156a25
ompatel-champion <ompatelupwork@gmail.com>
2022-03-02 08:11:14 +0800

    update agri

--- src/components/pages/AgricultureAndFoodSecurity.js
+++ src/components/pages/AgricultureAndFoodSecurity.js
@@ -22,29 +22,19 @@ class AgricultureAndFoodSecurity extends Component {
                             Agriculture And Food Security
                         </h3>
                         <p>
-                            Conglomerates and foreign investors have too much control over America’s food. During the
-                            height of the 2020 Covid-19 economic shutdown, many big food processors sent home their
-                            employees, preventing ranchers from getting their products to stores. Had ranchers been given
-                            local options to process their meat and produce, many local meat packing plants and
-                            processors could have enabled ranchers and farmers to keep grocery store shelves full and to
-                            make a living themselves. Our organization will seek to implement policy changes that will allow
-                            food to be locally processed and locally sold in order to help ranchers and farmers and secure
-                            the food supply
+                            Conglomerates and foreign investors have too much control over America’s food. During the height of the 2020 Covid-19 economic shutdown, many big food processors sent home their employees, preventing ranchers from getting their products to stores. Had ranchers been given local options to process their meat and produce, many local meat packing plants and processors could have enabled ranchers and farmers to keep grocery store shelves full and to make a living themselves. Our organization will seek to implement policy changes that will allow food to be locally processed and locally sold in order to help ranchers and farmers and secure the food supply.
                         </p>
                         <h5 className="w-100 mt-3">
-                            Policy Proposals
+                            Policy Recommendations
                         </h5>
                         <br />
                         <ul className="sagebrush-list">
-                            <li>- Implement a nationwide Food Freedom Act</li>
+                            <li>- Implement a nationwide Food Freedom Act.</li>
                             <li>- Increase domestic market competition for local producers, processors, and consumers</li>
-                            <li>- Enforce antitrust laws against oligopolies on meat and produce packing</li>
-                            <li>- Improve and enforce existing US code defining unlawful practices established under the
-                                Packers and Stockyards Act (1921)</li>
-                            <li>- Require country-of-origin labeling on all imported meats, including foreign meats
-                                processed in the United States
-                            </li>
-                            <li>- Pass the PRIME Act as it was introduced into the 116th Congress of the United States</li>
+                            <li>- Enforce antitrust laws against oligopolies on meat and produce packing.</li>
+                            <li>- Improve and enforce existing US code defining unlawful practices established under the Packers and Stockyards Act (1921).</li>
+                            <li>- Require country-of-origin labeling on all imported meats, including foreign meats processed in the United States.</li>
+                            <li>- Pass the PRIME Act as it was introduced into the 116th Congress of the United States.</li>
                         </ul>
                     </div>
                 </div>
